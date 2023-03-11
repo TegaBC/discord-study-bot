484dd76021f341d1f1b5d649ab270c19ace8c51e
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-03-11 17:10:06 +0000

    + util funcs

---
+++ db-funcs.js
@@ -0,0 +1,22 @@
+async function userHasRow(table, userId) {
+    const user = await table.findOne({ where: { userId: userId } })
+    return user
+}
+
+async function createUserRow(table, userId) {
+    try {
+        const user = await table.create({userId: userId})
+        return user
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+async function incrementPoints(table, userId, points) {
+    const user = await table.increment({points: points}, { where: {userId: userId} })
+    return user
+}
+
+module.exports = {
+    userHasRow, createUserRow, incrementPoints
+}
