5d5f5186055453445645b29152a3a8ed6bca6f8c
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-03-11 04:57:22 +0000

    core pomodoro code

---
+++ commands/study-timer.js
@@ -0,0 +1,71 @@
+const { SlashCommandBuilder } = require("discord.js");
+const wait = require('node:timers/promises').setTimeout;
+
+const replies = {
+    study: {
+        sessionStart: "Study is commencing",
+        sessionMiddle: "Midpoint of study has been reached.",
+        sessionEnd: "Study session has finished, well done."
+    },
+    break: {
+        sessionStart: "Break is starting, take a breather",
+        sessionMiddle: "Midpoint of break time has been reaeched.",
+        sessionEnd: "Break is over."
+    }
+}
+
+function oneDecimal(num) {
+    return Math.round(num * 10) / 10
+}
+
+const beginSession = async (interaction, length, study) => {
+    // save session in storage
+    interaction.client.sessionStorage[interaction.user.id] = {startTime: Date.now(), length: length, finishTime: Date.now() + length, study: study} // timestamp 
+
+    const channel = interaction.channel
+    const user = interaction.user
+
+    let replyOptions = study ? replies.study : replies.break
+
+    channel.send(`**SESSION** | ${replyOptions.sessionStart} | **${oneDecimal(length / 60_000)} minute session.** | ${user.toString()}`)
+    await wait(length / 2)
+    channel.send(`**SESSION** | ${replyOptions.sessionMiddle} | **${oneDecimal((length / 2) / 60_000)} minutes left.** | ${user.toString()}`)
+    await wait(length / 2)
+    channel.send(`**SESSION** | ${replyOptions.sessionEnd} | ${user.toString()}`)
+    await wait(500) // wait half a sec so it seems more fluid
+    delete interaction.client.sessionStorage[interaction.user.id]
+}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+    .setName("pomodoro")
+    .setDescription("Start a pomodoro study timer")
+    .addNumberOption(option =>
+        option.setName("study-length")
+        .setDescription("How long (in minutes), should the work session last?")
+        .setRequired(true)
+    )
+    .addNumberOption(option =>
+        option.setName("break-length")
+        .setDescription("How long (in minutes), should the break last?")
+        .setRequired(true)
+    )
+    .addNumberOption(option =>
+        option.setName("rounds")
+        .setDescription("How many rounds (study, break and then study) should be set?")
+    ),
+
+    async execute(interaction) {
+        const rounds = interaction.options.getNumber("rounds") ?? 1
+        const studyLength = interaction.options.getNumber("study-length") * 60_000
+        const breakLength = interaction.options.getNumber("break-length") * 60_000
+
+        await interaction.reply("**SESSION** | Initializing pomodoro session with" + interaction.user.toString());
+
+        for (let i = 1; i <= rounds; i++) {
+            await beginSession(interaction, studyLength, true)
+            await beginSession(interaction, breakLength, false)
+            await beginSession(interaction, studyLength, true)
+        }
+	},
+};
