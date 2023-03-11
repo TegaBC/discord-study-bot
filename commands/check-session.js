4c5eafe6f92b6ddd772b1bf42b5beb55c1d7451d
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-03-11 04:56:29 +0000

    returns time left

---
+++ commands/check-session.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require("discord.js")
+
+function oneDecimal(num) {
+    return Math.round(num * 10) / 10
+}
+
+module.exports = {
+	data: new SlashCommandBuilder().setName("check-session").setDescription("Checks time left on current session"),
+	async execute(interaction) {
+        const userStorage = interaction.client.sessionStorage[interaction.user.id]
+
+        if (userStorage) {
+            let timeLeft = (userStorage.finishTime -  Date.now()) / 60_000
+            await interaction.reply(`${interaction.user.toString()} has ${oneDecimal(timeLeft)} minutes left of their (${userStorage.study ? "study" : "break"}) session.`)
+        } 
+        else 
+        {
+            await interaction.reply(`${interaction.user.toString()} is currently not in a session.`)
+        }
+	},
+};
